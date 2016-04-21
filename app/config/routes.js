24b10031181eba856a60f5ca3fc191924afa88c5
Denis Pettens <denis.pettens@gmail.com>
2016-04-21 04:13:37 +0200

    Finish the server file and Begin the router

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,1 +1,7 @@
-//TODO
+const routes = require('express').Router();
+
+routes.get('/', (req, res) => {
+  res.status(200).json({ message: 'Connected!' });
+});
+
+module.exports = routes;
--- server.js
+++ server.js
@@ -1,14 +1,34 @@
 const express    = require('express');
-const app        = express();
 const bodyParser = require('body-parser');
+const morgan     = require('morgan');
 const routes     = require('./app/config/routes');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+/*
+ * Initial setup
+ */
+// Create an application express
+var app = express();
+
+// Use bodyParser middelware to get the data from a POST/PUT
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
+// Use morgan logger to log on the console
+app.use(morgan('dev'))
+
+// Set the port
 var port = process.env.PORT || 8080;
 
+/*
+ * Register routes form app/config/routes to /api
+ */
 app.use('/api', routes);
 
-app.listen(port);
-console.log('Server listening on port ' + port);
+/*
+ * Start the server
+ */
+app.listen(port, () => {
+  console.log('App listening on port 3000');
+});
