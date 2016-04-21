9594f274d8783a33367466bc1772f61cc7254a75
Denis Pettens <denis.pettens@gmail.com>
2016-04-21 06:24:00 +0200

    Update the sql file

--- MeetingHaters.sql
+++ MeetingHaters.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Mer 20 Avril 2016 à 23:53
+-- Généré le :  Jeu 21 Avril 2016 à 04:21
 -- Version du serveur :  5.5.47-MariaDB
 -- Version de PHP :  5.4.16
 
@@ -23,70 +23,70 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `m2m_user_meeting`
+-- Structure de la table `m2m_meetings_users`
 --
 
-CREATE TABLE IF NOT EXISTS `m2m_user_meeting` (
+CREATE TABLE IF NOT EXISTS `m2m_meetings_users` (
   `_id` int(11) NOT NULL,
-  `id_user` varchar(255) NOT NULL,
-  `id_meeting` varchar(255) NOT NULL,
-  `status` varchar(255) DEFAULT NULL,
-  `reason` varchar(255) DEFAULT NULL,
-  `expected_status` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id_user` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `id_meeting` int(11) NOT NULL,
+  `status` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `reason` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `expected_status` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `meeting`
+-- Structure de la table `meetings`
 --
 
-CREATE TABLE IF NOT EXISTS `meeting` (
-  `_id` varchar(255) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `location` varchar(255) DEFAULT NULL,
+CREATE TABLE IF NOT EXISTS `meetings` (
+  `_id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `location` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `time_pre` datetime DEFAULT NULL,
   `time_start` datetime DEFAULT NULL,
   `time_post` datetime DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `description` text COLLATE utf8_unicode_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `user`
+-- Structure de la table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
-  `mail` varchar(255) NOT NULL,
-  `firstname` varchar(255) DEFAULT NULL,
-  `surname` varchar(255) DEFAULT NULL,
-  `location` varchar(255) DEFAULT NULL,
+CREATE TABLE IF NOT EXISTS `users` (
+  `mail` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `firstname` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `surname` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `location` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `last_connection` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Index pour les tables exportées
 --
 
 --
--- Index pour la table `m2m_user_meeting`
+-- Index pour la table `m2m_meetings_users`
 --
-ALTER TABLE `m2m_user_meeting`
+ALTER TABLE `m2m_meetings_users`
   ADD PRIMARY KEY (`_id`),
   ADD KEY `id_user` (`id_user`),
   ADD KEY `id_meeting` (`id_meeting`);
 
 --
--- Index pour la table `meeting`
+-- Index pour la table `meetings`
 --
-ALTER TABLE `meeting`
+ALTER TABLE `meetings`
   ADD PRIMARY KEY (`_id`);
 
 --
--- Index pour la table `user`
+-- Index pour la table `users`
 --
-ALTER TABLE `user`
+ALTER TABLE `users`
   ADD PRIMARY KEY (`mail`);
 
 --
@@ -94,20 +94,25 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT pour la table `m2m_user_meeting`
+-- AUTO_INCREMENT pour la table `m2m_meetings_users`
 --
-ALTER TABLE `m2m_user_meeting`
+ALTER TABLE `m2m_meetings_users`
+  MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `meetings`
+--
+ALTER TABLE `meetings`
   MODIFY `_id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Contraintes pour les tables exportées
 --
 
 --
--- Contraintes pour la table `m2m_user_meeting`
+-- Contraintes pour la table `m2m_meetings_users`
 --
-ALTER TABLE `m2m_user_meeting`
-  ADD CONSTRAINT `m2m_user_meeting_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`mail`) ON DELETE CASCADE,
-  ADD CONSTRAINT `m2m_user_meeting_ibfk_2` FOREIGN KEY (`id_meeting`) REFERENCES `meeting` (`_id`) ON DELETE CASCADE;
+ALTER TABLE `m2m_meetings_users`
+  ADD CONSTRAINT `m2m_meetings_users_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`mail`) ON DELETE CASCADE,
+  ADD CONSTRAINT `m2m_meetings_users_ibfk_2` FOREIGN KEY (`id_meeting`) REFERENCES `meetings` (`_id`) ON DELETE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
