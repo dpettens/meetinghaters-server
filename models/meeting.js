a03c00544bef5cb92c3ad66ee597ee081bb660a4
Denis Pettens <denis.pettens@gmail.com>
2016-04-25 21:46:49 +0200

    Begin the meeting model

--- models/meeting.js
+++ models/meeting.js
@@ -1,1 +1,16 @@
-//TODO
+"use strict";
+var db = require('../libs/db');
+
+class Meeting {
+    constructor(meeting) {
+        this._id = meeting._id;
+        this.name = meeting.name;
+        this.location = meeting.location;
+        this.time_pre = meeting.time_pre;
+        this.time_start = meeting.time_start;
+        this.time_post = meeting.time_post;
+        this.description = meeting.description;
+    }
+}
+
+module.exports = Meeting;
